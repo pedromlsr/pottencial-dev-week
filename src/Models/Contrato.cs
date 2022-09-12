3020519c0cbc8580916cae35f6cdf6f4b3f9970a
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-09-12 17:15:04 -0300

    aula 1

---
+++ src/Models/Contrato.cs
@@ -0,0 +1,23 @@
+namespace src.Models;
+
+public class Contrato {
+    
+    public Contrato() {
+        this.DataCriacao = DateTime.Now;
+        this.Valor = 0;
+        this.TokenId = "000000";
+        this.Pago = false;
+    }
+
+    public Contrato(string TokenId, double Valor) {
+        this.DataCriacao = DateTime.Now;
+        this.TokenId = TokenId;
+        this.Valor = Valor;
+        this.Pago = false;
+    }
+
+    public DateTime DataCriacao { get; set; }
+    public string TokenId { get; set; }
+    public double Valor { get; set; }
+    public bool Pago { get; set; }
+}
